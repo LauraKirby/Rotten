ac64ad41f86610e3b631b4fba59d3d4e253a5314
Laura <laurakirby26@gmail.com>
2014-09-29 19:46:01 -0700

    first commit

---
+++ rotten/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  rotten
+//
+//  Created by Laura Kirby on 9/27/14.
+//  Copyright (c) 2014 Laura. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
